0c159a269ac397858f20cfe73dcf2cc08331cc31
Victor Volovik <andhope2die@gmail.com>
2015-07-07 18:42:52 +0300

    index and sign in pages added

--- config/view.php
+++ config/view.php
@@ -6,10 +6,10 @@
 
 	function display_messages($success_msg = NULL, $error_msg = NULL) {
 		echo "<div id='messages'>\n";
-		if(!is_null($success_msg)){
+		if(!is_null($success_msg) && (strlen($success_msg) > 0)) {
 			display_message($success_msg, SUCCESS_MESSAGE);
 		}
-		if(!is_null($error_msg)){
+		if(!is_null($error_msg) && (strlen($error_msg) > 0)) {
 			display_message($error_msg, ERROR_MESSAGE);
 		}
 		echo "</div>\n\n";
