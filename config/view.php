b04a8e6ed078b8c6ce9c601461733a8aa9d69e0c
Victor Volovik <andhope2die@gmail.com>
2015-07-11 16:07:18 +0300

    menu entry with link to show_users.php for admins only

--- config/view.php
+++ config/view.php
@@ -1,5 +1,7 @@
 <?php
+require_once "db_connection.php";
 require_once "app_config.php";
+require_once "authorize.php";
 
 const SUCCESS_MESSAGE = "success";
 const ERROR_MESSAGE = "error";
@@ -34,9 +36,9 @@ function display_head($page_title = "", $embedded_javascript = NULL) {
     <script type="text/javascript" src="js/jquery.validate.password.js"></script>
 EOD;
     if (!is_null($embedded_javascript)) {
-     echo "<script type='text/javascript'>" .
-     $embedded_javascript .
-     "</script>";
+      echo "<script type='text/javascript'>" .
+      $embedded_javascript .
+      "</script>";
    }
    echo " </head>";
  }
@@ -44,28 +46,51 @@ EOD;
  function display_title($title, $success_msg = NULL, $error_msg = NULL) {
   echo <<<EOD
   <body>
-   <div id="header">
-    <h1>PHP &amp; MySQL: The Missing Manual</h1>
-  </div>
-  <div id="example">
-   {$title}
- </div>
- <div id="menu">
-  <ul>
-     <li><a href="../index.html">Главная страница</a></li>
+    <div id="header">
+      <h1>PHP &amp; MySQL: The Missing Manual</h1>
+    </div>
+    <div id="example">
+      {$title}
+    </div>
+    <div id="menu">
+      <ul>
+        <li><a href="../index.html">Главная страница</a></li>
 EOD;
-  if (isset($_COOKIE['user_id'])) {
+   if (isset($_COOKIE['user_id'])) {
     $user_id = $_COOKIE['user_id'];
     echo "<li><a href='show_user.php?user_id={$user_id}'>Мой профиль</a></li>";
+    if(user_in_group($user_id, "admins")) {
+      echo "<li><a href='show_users.php'>Управление</a></li>";
+    }
     echo "<li><a href='signout.php'>Выйти</a></li>";
   } else {
     echo "<li><a href='signin.php'>Войти</a></li>";
   }
   echo <<< EOD
-    </ul>
-  </div>
+      </ul>
+    </div>
 EOD;
- display_messages($success_msg, $error_msg);
+display_messages($success_msg, $error_msg);
+}
+
+function user_in_group($user_id, $group) {
+  global $link;
+
+  $query_string =
+  "SELECT ug.user_id FROM user_groups ug, groups g
+  WHERE g.name = '%s' AND g.id = ug.group_id AND ug.user_id = " .
+  mysqli_real_escape_string($link, $_COOKIE['user_id']);
+
+  $query = sprintf($query_string,
+    mysqli_real_escape_string($link, $group),
+    mysqli_real_escape_string($link, $user_id));
+
+  $result = mysqli_query($link, $query);
+    if(mysqli_num_rows($result) == 1) {
+      return true;
+    } else {
+      return false;
+    }
 }
 
 function page_start($title, $javascript = NULL,
